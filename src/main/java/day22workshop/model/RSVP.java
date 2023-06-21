e875d59c8b902f01a847f10c35a91ed0547fbd19
daviniaaa <daviniaho99@gmail.com>
2023-06-21 14:09:08 +0800

    initial commit

---
+++ src/main/java/day22workshop/model/RSVP.java
@@ -0,0 +1,13 @@
+package day22workshop.model;
+
+import java.util.Date;
+
+
+public class RSVP {
+    private int id;
+    private String fullName;
+    private String Email;
+    private Integer phone;
+    private Date confirmDate;
+    private String comments;
+}
